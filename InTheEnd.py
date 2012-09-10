e570ffeb53dfd971347a82231b52bf04988fc66d
Pamela McA'Nulty <pamela@webreply.com>
2012-09-10 10:29:54 -0400

    o Get quiz working-ish
    Signed-off-by: Pamela McA'Nulty <pamela@webreply.com>

--- InTheEnd.py
+++ InTheEnd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+import csv
 import pygame
 import utils
 
@@ -18,10 +19,9 @@ class SpinImageSituation(utils.SituationBase):
         self.current_angle = 0
         self.rotations_left = rotations
         self.need_draw = True
-        self.done = False
 
     def rotate_image(self):
-        self.log("Rotations Left %s, current angle: %s" % (self.rotations_left, self.current_angle))
+        #self.log("Rotations Left %s, current angle: %s" % (self.rotations_left, self.current_angle))
         if self.current_angle==0:
             image = self.base_image
             rect = image.get_rect()
@@ -31,15 +31,6 @@ class SpinImageSituation(utils.SituationBase):
             rect.center = self.base_center
         return image, rect
         
-    def handle_event(self, event):
-        if event.type == pygame.QUIT:
-            self.next_situation_class = None
-            self.done = True
-        elif event.type==pygame.KEYDOWN:
-            self.done = True
-    
-    def still_running(self):
-        return not self.done
         
     def display(self):
         if self.need_draw:                        
@@ -58,29 +49,170 @@ class SpinImageSituation(utils.SituationBase):
                 self.need_draw = False
             pygame.display.flip()
 
-    def next_situation(self):
-        if self.next_situation_class:
-            return self.next_situation_class(self.g)
-        else:
-            return None
 
         
         
 class FirstNewspaperSituation(SpinImageSituation):
     def __init__(self, g):
-        
         SpinImageSituation.__init__(self, g, "test.png", SecondNewspaperSituation)
         
 class SecondNewspaperSituation(SpinImageSituation):
     def __init__(self, g):
-        SpinImageSituation.__init__(self, g, "test.png", None)
+        SpinImageSituation.__init__(self, g, "test.png", QuizSituation)
+    
         
+class QuizSummarySituation(utils.SituationBase):
+    def __init__(self, g):
+        utils.SituationBase.__init__(self, g)
+        self.FRAME_RATE = 5
+        self.background = pygame.Surface(self.g.screen.get_size()).convert()
+        self.background.fill((255, 255, 255))
+        self.g.screen.blit(self.background, (0, 0)) 
         
+        self.g.render_text("This is you:", utils.GameFont("monospace", 30, (0, 0, 0)), 50, 10)        
+        y = 60
+        for q, a in self.g.quiz_answers:        
+            self.g.render_text(q, utils.GameFont("monospace", 16, (0, 0, 0)), 50, y)
+            y += 18
+            self.g.render_text(a, utils.GameFont("monospace", 16, (30,148,89)), 60, y)
+            y += 25
+        
+        self.next_button = utils.ClickableText(self.g, "Next",
+                                       utils.GameFont("monospace", 20, (0,0,0)), 
+                                       200,y+40)
+        self.next_situation_class = FirstMainMap
+        
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            self.next_situation_class = None
+            self.done = True
+        elif event.type==pygame.MOUSEBUTTONUP:
+            mouse = pygame.mouse.get_pos()
+            if self.next_button.mouse_in_rect(mouse):
+                self.done = True
+        elif event.type==pygame.KEYDOWN:
+            self.done = True
+    
+    
+            
+class QuizSituation(utils.SituationBase):
+    questions = {}
+    
+    def __init__(self, g, q_num='1'):
+        utils.SituationBase.__init__(self, g)
+        self.FRAME_RATE = 5
+        if not QuizSituation.questions:
+            self.load_questions()
+        self.this_rec = QuizSituation.questions[q_num]
+        self.background = pygame.Surface(self.g.screen.get_size()).convert()
+        self.background.fill((255, 255, 255))
+        self.g.screen.blit(self.background, (0, 0)) 
+        self.log("Q: %s" % self.this_rec['Question'])
+        black_font = utils.GameFont("monospace", 20, (0,0,0))
+        self.unpressed_font = black_font
+        self.pressed_font = utils.GameFont("monospace", 20, (30,148,89))
+        self.g.render_text(self.this_rec['Question'], black_font, 50, 50)
+        self.responses = []
+        self.init_response("A", 50, 100)
+        self.init_response("B", 50, 130)
+        self.init_response("C", 50, 160)
+        self.answer = None
+        self.next_button = None
+        pygame.display.flip()
+    
+    def init_response(self, r, x, y):
+        name = "Response %s" % r
+        if self.this_rec[name]:
+            ct = utils.ClickableText(self.g, self.this_rec[name], 
+                                       self.unpressed_font, 
+                                       x, y)
+            ct.reply = self.this_rec["Answer to %s" % r]
+            self.responses.append(ct)
+            
+
+    def next_situation(self):
+        q_num = self.this_rec['Next Number']
+        if q_num in QuizSituation.questions:
+            return QuizSituation(self.g, q_num)
+        else:
+            return QuizSummarySituation(self.g)
+
+    def _click(self, mouse_up):        
+        mouse = pygame.mouse.get_pos()
+        if not self.answer:
+            return self._click_response(mouse, mouse_up)
+        else:
+            self._click_next(mouse, mouse_up)
+    
+    def _click_response(self, mouse, mouse_up):
+        answer = None
+        for ct in self.responses:
+            if ct.mouse_in_rect(mouse):
+                answer = ct
+                if ct.set_font(self.pressed_font):
+                    ct.render()
+            elif ct.set_font(self.unpressed_font):
+                ct.render()
+                
+        if answer:
+            self.log("Clicked Answer: %s" % answer.text)
+        if mouse_up and answer:
+            self.log("mouse up %s" % answer.text)
+            self.answer = answer
+            if answer.reply:
+                self.g.render_text(answer.reply, utils.GameFont("monospace", 20, (153,128,18)), 50, 200)
+            self.g.add_quiz_answer(self.this_rec['Question'], answer.text)
+            self.next_button = utils.ClickableText(self.g, "Next",
+                                       utils.GameFont("monospace", 20, (0,0,0)), 
+                                       200,250)
+                                       
+    def _click_next(self, mouse, mouse_up):
+        if self.next_button.mouse_in_rect(mouse):
+            if mouse_up:
+                self.done = True
+            elif self.next_button.set_font(utils.GameFont("monospace", 20, (80,80,80))):
+                self.next_button.render()
+                
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            self.next_situation_class = None
+            self.done = True
+        elif event.type in (pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP):
+            self._click(event.type==pygame.MOUSEBUTTONUP)
+            # TODO: trigger next situation...
+        elif event.type==pygame.KEYDOWN and self.next_button:
+            self.done = True
+
+        
+    def load_questions(self):
+        f = open("InterviewQuiz.csv", "rU")
+        try:
+            reader = csv.reader(f)
+            header = reader.next()
+            for row in reader:
+                rec = dict(map(None, header, row))
+                if rec['Number']:
+                    QuizSituation.questions[rec['Number']] = rec
+        finally:
+            f.close()
+
+# TODO: layout blocks...
+
 class InTheEndGame(utils.GameBase):
     def __init__(self):
         utils.GameBase.__init__(self)
-        self.screen = pygame.display.set_mode((600,600),1)
+
+        DISPLAY_SIZE = (600, 600)
+        DISPLAY_MODE = 1
+        self.init_display(DISPLAY_SIZE, DISPLAY_MODE)
         #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
+        self.quiz_answers = []
+        self.quiz_by_q = {}
+    
+    def add_quiz_answer(self, q, a):
+        self.quiz_answers.append([q, a])
+        self.quiz_by_q[q] = a
 
 
     def firstSituation(self):
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import time
 import pygame
 
@@ -8,11 +9,176 @@ def _log(msg, verbosity=1):
     if verbosity<=_verbosity:
         print msg
 
+def load_image(file_name, colorkey=None):
+    fullname = os.path.join(file_name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', fullname
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image
+
+class Button(object):
+    def __init__(self, g, normal_image, pressing_image, pressed_image, x, y, name):
+        self.g = g
+        self.x = x
+        self.y = y
+        self.images = {}
+        self.images['normal'] = normal_image
+        self.images['pressing'] = pressing_image
+        self.images['pressed'] = pressed_image
+        self.name = name
+        self.state = None
+        self.set_state("normal")
+        
+    def set_state(self, state):
+        if self.state!=state:
+            self.state = state
+            rect = self.images[state].get_rect()
+            rect.topleft = self.x, self.y
+            self.right, self.bottom = rect.bottomright
+            print "BUTTON: ", self.x, self.y, self.right, self.bottom
+            self.draw()
+        
+    def draw(self):
+        self.g.screen.blit(self.images[self.state], (self.x, self.y))
+            
+    def mouse_down(self, mouse):
+        if self.mouse_in_button(mouse):
+            self.set_state("pressing")
+            return True
+        else:
+            return False
+    
+    def mouse_up(self, mouse):
+        if self.mouse_in_button(mouse):
+            self.set_state("pressed")
+            return True
+        else:
+            return False
+    
+    def mouse_in_button(self, mouse):
+        x,y = mouse
+        return  x>self.x and x<self.right and \
+           y>self.y and y<self.bottom
+
+# http://pygame.org/wiki/Spritesheet
+class SpriteSheet(object):
+    def __init__(self, filename):
+        try:
+            self.sheet = pygame.image.load(filename).convert_alpha()
+        except pygame.error, message:
+            print 'Unable to load spritesheet image:', filename
+            raise SystemExit, message
+    # Load a specific image from a specific rectangle
+    def image_at(self, rectangle, colorkey = None):
+        "Loads image from x,y,x+offset,y+offset"
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size, flags=pygame.SRCALPHA)
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+    # Load a whole bunch of images and return them as a list
+    def images_at(self, rects, colorkey = None):
+        "Loads multiple images, supply a list of coordinates" 
+        return [self.image_at(rect, colorkey) for rect in rects]
+    # Load a whole strip of images
+    def load_strip(self, rect, image_count, colorkey = None):
+        "Loads a strip of images and returns them as a list"
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
+
+    def image_at_index(self, row, col, w, h, colorkey=None):
+        rect = (col*w, row*h, (col*w)+w, (row*h)+h)
+        print "RECT:", rect, self.sheet.get_rect()
+        return self.image_at(rect, colorkey)
+    
+class RadioButtons(object):
+    def __init__(self, g, tileset, locations=None):
+        self.g = g
+        self.buttons = []
+        self.by_name = {}
+        self.pressed = None
+        tiles = SpriteSheet(tileset)
+                
+        self.normal_image = tiles.image_at_index(0, 0, 16, 16)
+        self.pressing_image = tiles.image_at_index(0, 1, 16, 16)
+        self.pressed_image = tiles.image_at_index(0, 2, 16, 16)
+        
+        if locations:
+            for x, y, name in locations:
+                self.add_button(x, y, name)
+    
+    def add_button(self, x, y, name):
+        b = Button(self.g, self.normal_image, self.pressing_image, self.pressed_image, x, y, name)
+        self.buttons.append(b)
+        self.by_name[name] = b
+        
+    def mouse_down(self, mouse):
+        print "RADIO MOUSE DOWN:", mouse
+        for button in self.buttons:
+            if button.mouse_down(mouse):
+                self.pressed = button
+            else:
+                button.set_state("normal")
+
+    def mouse_up(self, mouse):
+        print "RADIO MOUSE UP:", mouse
+        for button in self.buttons:
+            if button.mouse_up(mouse):
+                self.pressed = button
+            else:
+                button.set_state("normal")
+    
+    def draw(self):
+        for button in self.buttons:
+            button.draw()
+
+class ClickableText(object):
+    def __init__(self, g, text, font, x, y):
+        self.g = g
+        self.text = text
+        self.font = font
+        self.x, self.y = x, y
+        self.render()
+        
+    def render(self):
+        text, self.rect = self.g.render_text(self.text, self.font, self.x, self.y)
+        
+    def mouse_in_rect(self, mouse):
+        return self.rect.collidepoint(mouse)
+    
+    def set_font(self, font):
+        if self.font!=font:
+            self.font = font
+            return True
+        else:
+            return False
+            
+class GameFont(object):
+    def __init__(self, name, size, color):
+        self.name = name
+        self.size = size
+        self.color = color
+        self.font = pygame.font.SysFont(name, size, True, False)
+        
 class GameBase(object):
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
         self.get_options()
+    
+    def init_display(self, display_size, display_mode):
+        self.screen = pygame.display.set_mode(display_size, display_mode)
         
     def get_options(self):
         # TODO: support optparse for more detailed options
@@ -24,6 +190,15 @@ class GameBase(object):
     def firstSituation(self):
         return None
         
+    def render_text(self, s, game_font, x, y):
+        antialias = True
+        text = game_font.font.render(s, antialias, game_font.color)
+        textRect = text.get_rect()
+        textRect.left = self.screen.get_rect().left+x
+        textRect.top = self.screen.get_rect().top+y
+        self.screen.blit(text, textRect) 
+        return text, textRect
+        
     def quit(self):
         pygame.quit()
 
@@ -33,6 +208,8 @@ class SituationBase(object):
     def __init__(self, game_obj):
         self.g = game_obj
         self.FRAME_RATE = 30
+        self.next_situation_class = None
+        self.done = False
         self.log("init")
     
     def log(self, msg):
@@ -49,27 +226,34 @@ class SituationBase(object):
             self.do_stuff_before_display()
             self.display()
             self.do_stuff_after_display()
-        actual_frame_rate = ticks/(time.time()-start)
+        actual_frame_rate = ticks/(time.time()-start)        
         self.log("Requested frame rate: %0.2f, actual frame rate: %0.2f" % (self.FRAME_RATE, actual_frame_rate))
         return self.next_situation()
     
     def still_running(self):
-        return True
+        return not self.done
         
     def handle_event(self, event):
-        pass
+        if event.type == pygame.QUIT:
+            self.next_situation_class = None
+            self.done = True
+        elif event.type==pygame.KEYDOWN:
+            self.done = True
     
     def do_stuff_before_display(self):
         pass
         
     def display(self):
-        pass
+        pygame.display.flip()
         
     def do_stuff_after_display(self):
         pass
     
     def next_situation(self):
-        return None
+        if self.next_situation_class:
+            return self.next_situation_class(self.g)
+        else:
+            return None
 
 def main(game_class):
     gc = game_class()
